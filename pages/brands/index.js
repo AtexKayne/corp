43b0f5a4dc450839b2201680320c09e8ea6fb2e5
Alex Kremlev <alex.kremlev@videinfra.com>
2022-09-20 01:31:52 +0500

    fix path

--- pages/brands/index.js
+++ pages/brands/index.js
@@ -1,20 +1,20 @@
-import Cloud from "../../components/Cloud"
-import KaleidoscopeElem from "../../components/KaleidoscopeElem"
-import Title from "../../components/Title"
-import MainLayout from "../../layout/MainLayout"
-import { brandItems } from './helpers/constants'
+import Cloud from '../../components/Cloud'
+import KaleidoscopeElem from '../../components/KaleidoscopeElem'
+import Title from '../../components/Title'
+import MainLayout from '../../layout/MainLayout'
+import { brandItems } from '../../components/helpers/constants'
 
 export default function Brands({ items }) {
     return (
         <MainLayout className='ui-light'>
-            <div className="col col--between">
+            <div className='col col--between'>
                 <Title text='Brands' hover='Бренды'/>
                 <div className='text--c3'>
                     14 брендов лучшей косметики для парикмахеров, стилистов, колористов
                 </div>
                 <Cloud items={items} />
             </div>
-            <div className="col col--center">
+            <div className='col col--center'>
                 <KaleidoscopeElem />
             </div>
         </MainLayout>
@@ -23,12 +23,12 @@ export default function Brands({ items }) {
 
 export async function getServerSideProps({req}) {
     let resp, json
-    try {
-        resp = await fetch(`${process.env.API_URL}/brands`)
-        json = await resp.json()
-    } catch (error) {
+    // try {
+    //     resp = await fetch(`${process.env.API_URL}/brands`)
+    //     json = await resp.json()
+    // } catch (error) {
         json = brandItems
-    }
+    // }
   
     return {
         props: {
