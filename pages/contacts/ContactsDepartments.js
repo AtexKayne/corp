aa4c52843b348f6e3a1c3d51d29ce799a2a426b3
Alex Kremlev <alex.kremlev@videinfra.com>
2022-10-26 16:12:29 +0500

    update contacts page pt 2

--- pages/contacts/ContactsDepartments.js
+++ pages/contacts/ContactsDepartments.js
@@ -1,27 +1,37 @@
 import style from '../../styles/module/contacts/contacts-departments.module.scss'
-import { useRef, useEffect } from "react"
-import { motion, useAnimationControls, useInView } from "framer-motion"
+import { useRef, useEffect } from 'react'
+import { motion, useAnimationControls, useInView } from 'framer-motion'
 
 function Line({ department = {} }) {
     const refLine = useRef(null)
     const isInView = useInView(refLine, { margin: '100px 100px 150px 200px' })
     const animateLine = useAnimationControls()
-    const animateText = useAnimationControls()
+    const animateTextA = useAnimationControls()
+    const animateTextB = useAnimationControls()
+    const animateTextC = useAnimationControls()
+    const animateCube = useAnimationControls()
+    
 
     useEffect(() => {
         if (isInView) {
-            animateText.start({ opacity: 1, transition: { duration: 1.5, delay: 0.5 } })
+            animateCube.start({ opacity: 1, transition: { duration: 0.2, delay: 0.5 } })
+            animateTextA.start({ opacity: 1, transition: { duration: 1, delay: 0.5 } })
+            animateTextB.start({ opacity: 1, transition: { duration: 1, delay: 1.0 } })
+            animateTextC.start({ opacity: 1, transition: { duration: 1, delay: 1.5 } })
             animateLine.start({ width: '100%', transition: { duration: 1.5, delay: 0.5 } })
         } else {
-            animateText.start({ opacity: 0, transition: { duration: 1.5, delay: 0.5 } })
-            animateLine.start({ width: '0%', transition: { duration: 1.5, delay: 0.5 } })
+            animateCube.start({ opacity: 0, transition: { duration: 0.2 } })
+            animateTextA.start({ opacity: 0, transition: { duration: 1 } })
+            animateTextB.start({ opacity: 0, transition: { duration: 1 } })
+            animateTextC.start({ opacity: 0, transition: { duration: 1 } })
+            animateLine.start({ width: '0%', transition: { duration: 1.5 } })
         }
     }, [isInView])
 
     return (
         <div ref={refLine} className={`${style.line} mb-1.5`}>
-            <motion.div animate={animateText} className='text--h4 mb-1'>{department.name}</motion.div>
-            <div className={style.lineImage}>
+            <motion.div animate={animateTextA} className='text--h4 mb-1'>{department.name}</motion.div>
+            <motion.div animate={animateCube} className={style.lineImage}>
                 <motion.svg animate={animateLine} id='patternId' width='100%' height='100%'>
                     <defs>
                         <pattern id='a' patternUnits='userSpaceOnUse' width='40' height='20' patternTransform='scale(2) rotate(0)'>
@@ -30,11 +40,11 @@ function Line({ department = {} }) {
                     </defs>
                     <rect width='100%' height='40' transform='translate(-15, -12)' fill='url(#a)' />
                 </motion.svg>
-            </div>
-            <motion.span animate={animateText} className='text--t1'>
+            </motion.div>
+            <motion.span animate={animateTextB} className='text--t1'>
                 <a href={`tel:${department.phone}`} className='c-hover'>{department.phone}</a>
             </motion.span>
-            <motion.span animate={animateText} className='text--t1 text--transparent'>
+            <motion.span animate={animateTextC} className='text--t1 text--transparent'>
                 <a href={`mail:${department.mail}`} className='c-hover'>{department.mail}</a>
             </motion.span>
         </div>
