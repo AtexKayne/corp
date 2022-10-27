1eac7a18e81accac1c84a8129ccf2a2d6a399c21
Alex Kremlev <alex.kremlev@videinfra.com>
2022-10-28 03:28:54 +0500

    update brands page pt 3

--- pages/brands/BrandList.js
+++ pages/brands/BrandList.js
@@ -9,13 +9,14 @@ import useDeviceDetect from '../../components/helpers/useDeviceDetect'
 
 export default function BrandList({ items, tags }) {
     const [sliderActive, setSliderActive] = useState(false)
+    const [scrollHeight, setScrollHeight] = useState(-100)
     const [scrollWidth, setScrollWidth] = useState(-100)
     const [isDragging, setIsDragging] = useState(false)
     const [activeElem, setActiveElem] = useState(false)
     const [tagActive, setTagActive] = useState([])
     const [elements, setElements] = useState(items)
     const [letter, setLetter] = useState('AZ')
-    
+
     const steps = items ? items.length : 1
     const refSlider = useRef(null)
     const refContent = useRef(null)
@@ -23,6 +24,8 @@ export default function BrandList({ items, tags }) {
     const refSliderItem = useRef(null)
     const refCurrentItem = useRef(null)
     const refTagContainer = useRef(null)
+    const refSliderContent = useRef(null)
+    const refIsContentScroll = useRef(false)
     const refIsTitleShown = useRef('start')
     const { isMobile } = useDeviceDetect()
     const animateItemY = useMotionValue(0)
@@ -44,7 +47,7 @@ export default function BrandList({ items, tags }) {
         animateItem.start({ y: position, transition: { type: 'tween' } })
     }
 
-    const remoteTagHandler = () => {
+    const remoteTagClickHandler = () => {
         animateTagInner.start({ x: 0 })
         animateTagRemote.start({ x: -100, opacity: 0, pointerEvents: 'none', transition: { duration: 0.5 } })
     }
@@ -54,6 +57,14 @@ export default function BrandList({ items, tags }) {
         const innerElements = Array.from(refTagInner.current.childNodes)
         innerElements.forEach(el => scrollWidth += el.offsetWidth + 20)
         setScrollWidth(refTagContainer.current.clientWidth - scrollWidth)
+    }
+
+    const setContainerScrollHeight = () => {
+        let scrollHeight = 0
+        const innerElements = Array.from(refSliderContent.current.childNodes)
+        innerElements.forEach(el => scrollHeight += el.offsetHeight + 20)
+        setScrollWidth(refTagContainer.current.clientHeight - scrollHeight)
+        setScrollHeight(scrollHeight)
     }
 
     const tagClickHandler = (index) => {
@@ -70,12 +81,57 @@ export default function BrandList({ items, tags }) {
         setTagScrollWidth()
     }
 
+    const containerStartDragHandler = () => {
+        setActiveElem(false)
+        animateBlock.start({ y: 0, height: '0px', transition: { duration: 1 } })
+    }
+
+    const containerEndDragHandler = () => {
+        setTimeout(() => {
+            const transform = refContent.current.style.transform
+            const position = +transform.split('translateY(')[1].split('px)')[0]
+            if (position <= -50 && position > -150 && refIsTitleShown.current !== 'next') startAnimateTags()
+        }, 300)
+    }
+
+    const containerDragHandler = () => {
+        if (refIsContentScroll.current) return
+        refIsContentScroll.current = true
+        const transform = refContent.current.style.transform
+        const position = +transform.split('translateY(')[1].split('px)')[0]
+        if (position <= -50 && position > -150 && refIsTitleShown.current !== 'next') startAnimateTags()
+        else if (position <= -150 && refIsTitleShown.current !== 'end') endAnimateTags()
+        else if (position > -50 && refIsTitleShown.current !== 'start') showAnimateTags()
+        setTimeout(() => refIsContentScroll.current = false, 100)
+    }
+
+    const startAnimateTags = () => {
+        refIsTitleShown.current = 'next'
+        animateNav.start({ y: isMobile ? 0 : -40, transition: { duration: 0.4 } })
+        animateTitle.start({ y: isMobile ? -60 : -40, transition: { duration: 0.4 } })
+        animateTagsContainer.start({ y: -80, opacity: 0, pointerEvents: 'none', transition: { duration: 0.4 } })
+    }
+
+    const endAnimateTags = () => {
+        refIsTitleShown.current = 'end'
+        animateNav.start({ y: -200, transition: { duration: 0.4 } })
+        animateTitle.start({ y: -200, transition: { duration: 0.4 } })
+    }
+
+    const showAnimateTags = () => {
+        refIsTitleShown.current = 'start'
+        animateNav.start({ y: 0, transition: { duration: 0.4 } })
+        animateTitle.start({ y: 0, transition: { duration: 0.4 } })
+        animateTagsContainer.start({ y: 0, opacity: 1, pointerEvents: 'all', transition: { duration: 0.4 } })
+    }
+
     const moveDrag = event => {
         const index = Math.floor(event.y / ((refSlider.current.clientHeight - 20) / steps))
         const currentElement = elements[index]
         animateItemY.set(event.y)
 
         if (currentElement && refCurrentItem.current !== currentElement.name && sliderActive) {
+            const titlePosition = refIsTitleShown.current
             refCurrentItem.current = currentElement.name
 
             setActiveElem(currentElement)
@@ -88,37 +144,25 @@ export default function BrandList({ items, tags }) {
                 }
             })
 
-            if (refIsTitleShown.current === 'start' || (refIsTitleShown.current === 'end' && index < 3)) {
-                refIsTitleShown.current = 'next'
-                animateNav.start({ y: isMobile ? 0 : -40, transition: { duration: 1 } })
-                animateTitle.start({ y: isMobile ? -60 : -40, transition: { duration: 1 } })
-                animateBlock.start({ y: isMobile ? 220 : 260, height: isMobile ? '50vh' : '290px', transition: { duration: 1 } })
-                animateTagsContainer.start({ y: -80, opacity: 0, pointerEvents: 'none', transition: { duration: 1 } })
-            }
+            if (titlePosition === 'start' || (titlePosition === 'end' && index < 3)) startAnimateTags()
+            else if (titlePosition === 'next' && index >= 3) endAnimateTags()
 
-            if (refIsTitleShown.current === 'next' && index >= 3) {
-                refIsTitleShown.current = 'end'
-                animateNav.start({ y: -200, transition: { duration: 1 } })
-                animateTitle.start({ y: -200, transition: { duration: 1 } })
-            }
+            animateBlock.start({ y: isMobile ? 220 : 260, height: isMobile ? '50vh' : '290px', transition: { duration: 1 } })
 
         } else if (!elements[index] && letter !== 'AZ') {
             refCurrentItem.current = false
             setLetter('AZ')
-            animateNav.start({ y: 0, transition: { duration: 1 } })
-            animateTitle.start({ y: 0, transition: { duration: 1 } })
-            animateBlock.start({ y: 0, height: '0px', transition: { duration: 1 } })
-            animateTagsContainer.start({ y: 0, opacity: 1, pointerEvents: 'all', transition: { duration: 1 } })
-            refIsTitleShown.current = 'start'
+            showAnimateTags()
             setActiveElem(false)
             setSliderActive(false)
+            animateBlock.start({ y: 0, height: '0px', transition: { duration: 1 } })
         }
     }
 
     const tagsEndDrag = () => {
         const transform = refTagInner.current.style.transform
         const position = +transform.split('translateX(')[1].split('px)')[0]
-        if (transform === 'none' || position >= 0) {
+        if (!isMobile && (transform === 'none' || position >= 0)) {
             animateTagRemote.start({ x: -100, opacity: 0, pointerEvents: 'none', transition: { duration: 0.5 } })
         }
         setTimeout(() => setIsDragging(false), 50)
@@ -126,7 +170,7 @@ export default function BrandList({ items, tags }) {
 
     const tagsStartDrag = () => {
         setIsDragging(true)
-        animateTagRemote.start({ x: 0, opacity: 1, pointerEvents: 'all', transition: { duration: 0.2 } })
+        if (!isMobile) animateTagRemote.start({ x: 0, opacity: 1, pointerEvents: 'all', transition: { duration: 0.2 } })
     }
 
     const wheelHandler = event => {
@@ -150,7 +194,7 @@ export default function BrandList({ items, tags }) {
         }))
 
         setTagScrollWidth()
-
+        if (isMobile) setContainerScrollHeight()
     }, [items])
     return (
         <div className={style.brandListWrapper}>
@@ -164,7 +208,7 @@ export default function BrandList({ items, tags }) {
                     Бренды
                 </motion.h1>
                 <motion.div ref={refTagContainer} animate={animateTagsContainer} data-dragging={isDragging} className={style.brandListTagsContainer}>
-                    <motion.div onClick={remoteTagHandler} animate={animateTagRemote} initial={{ x: -100, opacity: 0, pointerEvents: 'none' }} className={`${style.brandListTagRemote} c-hover`} />
+                    <motion.div onClick={remoteTagClickHandler} animate={animateTagRemote} initial={{ x: -100, opacity: 0, pointerEvents: 'none' }} className={`${style.brandListTagRemote} c-hover`} />
                     <motion.div
                         drag='x'
                         ref={refTagInner}
@@ -186,7 +230,7 @@ export default function BrandList({ items, tags }) {
 
                 <div ref={refSlider} data-active={sliderActive} className={`${style.brandListSlider}`}>
                     <motion.div
-                        drag="y"
+                        drag='y'
                         ref={refSliderItem}
                         onUpdate={moveDrag}
                         dragMomentum={false}
@@ -200,8 +244,16 @@ export default function BrandList({ items, tags }) {
                     <div className={style.brandListSliderLine}></div>
                 </div>
 
-                <div onWheel={wheelHandler} className={style.brandListSliderContent}>
-                    <motion.div animate={animateScroll} ref={refContent} className={style.brandListSliderContentInner}>
+                <div ref={refSliderContent} onWheel={wheelHandler} className={style.brandListSliderContent}>
+                    <motion.div
+                        drag={isMobile ? 'y' : 'none'}
+                        onPan={isMobile ? containerDragHandler : null}
+                        onPanStart={isMobile ? containerStartDragHandler : null}
+                        onPanEnd={isMobile ? containerEndDragHandler : null}
+                        dragConstraints={{ top: scrollHeight, bottom: 0 }}
+                        animate={animateScroll}
+                        ref={refContent}
+                        className={style.brandListSliderContentInner}>
                         {elements ?
                             elements.map((item, index) => (
                                 <div
@@ -210,7 +262,14 @@ export default function BrandList({ items, tags }) {
                                     onClick={() => clickHandler(index, activeElem === item)}
                                     className={`${style.brandListSliderContentItem} c-hover`}>
                                     <div className={`${style.brandListSliderContentItemTitle} text--c2`}>{item.name}</div>
-                                    <A externalClass={`${style.brandListSliderContentItemLink} text--h1`} href={`brand/${item.name}`} text={item.name} />
+                                    <div className={`${style.brandListSliderContentItemLink} c-hover`}>
+                                        <A externalC href={`brand/${item.name}`} text={item.name} />
+                                        <svg width='61' height='16' viewBox='0 0 61 16' fill='none'>
+                                            <line y1='10' x2='30' y2='10' stroke='currentColor' strokeWidth='2' />
+                                            <line x1='30' y1='8' x2='60' y2='8' stroke='currentColor' strokeWidth='2' />
+                                            <path d='M60.7071 8.70711C61.0976 8.31658 61.0976 7.68342 60.7071 7.29289L54.3431 0.928932C53.9526 0.538408 53.3195 0.538408 52.9289 0.928932C52.5384 1.31946 52.5384 1.95262 52.9289 2.34315L58.5858 8L52.9289 13.6569C52.5384 14.0474 52.5384 14.6805 52.9289 15.0711C53.3195 15.4616 53.9526 15.4616 54.3431 15.0711L60.7071 8.70711ZM59 9H60V7H59V9Z' fill='currentColor' />
+                                        </svg>
+                                    </div>
                                     <div className={style.brandListSliderContentItemText}>{item.text}</div>
                                     <div className={style.brandListSliderContentItemImage}>
                                         <Image src={item.logo} alt={item.name} width='100%' height='100%' />
