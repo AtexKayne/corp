3a8eecd2ab36b699300e3692d6651db4e27ae644
Alex Kremlev <alex.kremlev@videinfra.com>
2022-11-02 12:53:42 +0500

    brand page fixes

--- pages/brand/BrandImage.js
+++ pages/brand/BrandImage.js
@@ -1,16 +1,22 @@
 import style from '../../styles/module/brand/brand-image.module.scss'
 import { SmoothScrollContext } from '../../components/helpers/SmoothScroll.context'
-import { useContext } from 'react'
+import { useContext, useEffect, useState, useRef } from 'react'
 
 export default function BrandImage({ image }) {
     const { scroll } = useContext(SmoothScrollContext)
     const scrollHandler = event => {
         event.deltaY > 0 && scroll && scroll.scrollTo('#about')
     }
+    const [leftPosition, setLeftPosititon] = useState(0)
+    const refImage = useRef()
+    useEffect(() => {
+        const offsetX = refImage.current.getBoundingClientRect().x
+        setLeftPosititon(-offsetX)
+    }, []);
         
     return (
         <section id='image' data-scroll-section className='p-relative'>
-            <div onWheel={scrollHandler} className={style.image} style={{ backgroundImage: `url(${image})` }}>
+            <div ref={refImage} onWheel={scrollHandler} className={style.image} style={{ backgroundImage: `url(${image})`, transform: `translateX(${leftPosition}px)` }}>
 
             </div>
         </section>
