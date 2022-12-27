5f2df25513eade8e1499f9d6080d638d545fee42
Alex Kremlev <alex.kremlev@videinfra.com>
2022-12-27 13:10:25 +0500

    minor fix

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
+import Link from "next/link";
 import MainLayout from "../layout/MainLayout";
 
 export default function Home() {
   return (
     <MainLayout title='Главная'>
-      <h1>MAIN sss</h1>
+      <div className='pt-3' />
+      <h1>Главная страница</h1>
+      <div className='pt-3' />
+      <div className='pl-2'>
+        <div className='text--t1'>Список страниц:</div>
+        <div className='pt-1' />
+        <Link href='/product/test'>
+          <a className='link'>Карточка товара</a>
+        </Link>
+      </div>
+      <div className='pt-5' />
     </MainLayout>
   )
 }
