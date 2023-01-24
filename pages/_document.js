b90fb6f3f5c881884a08663c7a55d7ef1098e022
Alex Kremlev <alex.kremlev@videinfra.com>
2023-01-24 15:03:44 +0500

    minor fix

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html lang='ru'>
-            <Head />
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+            </Head>
             <body>
                 <Main />
                 <NextScript />
