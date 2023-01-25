7b8ff3c612e53c26a0e46a4cb62109563031d518
Alex Kremlev <alex.kremlev@videinfra.com>
2023-01-25 17:26:06 +0500

    fix pt 4

--- components/usefull/templates/ModalColors.js
+++ components/usefull/templates/ModalColors.js
@@ -25,7 +25,9 @@ export default function ModalColors() {
                         ? variants.map(variant => (
                             <Link key={variant.link} href={`/${variant.link}`}>
                                 <div data-status={variant.status} className={style.variant}>
-                                    <Image className={style.image} src={variant.image} width='90' height='90' alt='' />
+                                    <div className={style.image}>
+                                        <Image src={variant.image} width='90' height='90' alt='' />
+                                    </div>
                                     <div className={style.text}>
                                         <div className='text--bold text--t1 pb-0.5'>{variant.name}</div>
                                         <div className={`${variant.status !== 'current' ? 'text--normal' : ''} text--t5`}>{variant.text}</div>
