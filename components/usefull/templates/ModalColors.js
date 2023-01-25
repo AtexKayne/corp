315d27f1bc987f3ffdc045805697437bf4da15aa
Alex Kremlev <alex.kremlev@videinfra.com>
2023-01-25 17:23:05 +0500

    fix pt 3

--- components/usefull/templates/ModalColors.js
+++ components/usefull/templates/ModalColors.js
@@ -2,6 +2,7 @@ import { useState, useEffect, useRef } from 'react'
 import { productVariants } from '../../helpers/constants'
 import style from '../../../styles/module/usefull/templates/Modal-colors.module.scss'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function ModalColors() {
     const [title, setTitle] = useState('')
@@ -24,7 +25,7 @@ export default function ModalColors() {
                         ? variants.map(variant => (
                             <Link key={variant.link} href={`/${variant.link}`}>
                                 <div data-status={variant.status} className={style.variant}>
-                                    <img className={style.image} src={variant.image} width='90' height='90' alt='' />
+                                    <Image className={style.image} src={variant.image} width='90' height='90' alt='' />
                                     <div className={style.text}>
                                         <div className='text--bold text--t1 pb-0.5'>{variant.name}</div>
                                         <div className={`${variant.status !== 'current' ? 'text--normal' : ''} text--t5`}>{variant.text}</div>
